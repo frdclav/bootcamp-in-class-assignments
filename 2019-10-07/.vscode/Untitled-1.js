855fbb142c6bb039c93e4bafeaeabc21d14632e5
Francis Rodson Clavel <frdclav@gmail.com>
2019-10-07 20:29:19 -0700

    adds 2019-10-07

---
+++ 2019-10-07/.vscode/Untitled-1.js
@@ -0,0 +1,11 @@
+function returnBigger(x, y) {
+    if (x > y) {
+        return x
+    } else {
+        return y
+    }
+}
+console.log(returnBigger(1, 2))
+console.log(returnBigger(6, -4))
+console.log(returnBigger(3.4, 2))
+console.log("HellO")
